9687efdd7c4e7b37d7985ae2381a99b2bb4af061
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-09-10 22:46:27 -0700

    Implementing chaining unsuccessfully

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -9,12 +9,41 @@
  */
 
 var fs = require('fs');
+var request = require('request');
 var Promise = require('bluebird');
 
-
+Promise.promisifyAll(fs);
+Promise.promisifyAll(request);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  // (1) to get Github username, invoke pluckFirstLine
+  console.log('Get Github name');
+  return fs.readFile(readFilePath)
+    .then(function(fileData) {
+      if (fileData) {
+        var dataString = fileData.toString();
+        var endOfLine = dataString.search(/$/gm);
+        var githubName = dataString.slice(0, endOfLine);
+        // console.log(githubName);
+        return githubName;
+      } else {
+        throw new Error;
+      }
+    })
+  // (2) to get profile, invoke getGitHubProfile
+    .then(function(newGithubName) {
+      var options = {
+        url: 'https://api.github.com/users/' + newGithubName,
+        headers: { 'User-Agent': 'request' },
+        json: true
+      };
+      console.log(request.get(options));
+      return request.get(options);
+    })
+  // (3) then JSON parse and invoke fs.writeFile
+    .then(function(githubProfile) {
+      return fs.writeFile(writeFilePath, githubProfile);
+    });
 };
 
 // Export these functions so we can test them
